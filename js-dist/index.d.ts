3bb16ad7f4fe3c34eddae57eca2c173f5f2fc1fb
Ryan Pell <ryan@ryanpell.co.uk>
2023-05-02 12:48:23 +0100

    updated rollup

--- js-dist/index.d.ts
+++ js-dist/index.d.ts
@@ -4,7 +4,10 @@
  * @param {string} connection This is a optional connection string that you can use to connect to another database that is not the default. The default is the one that is defined within the Tauri setup. The string should be used as as a JDBC string (starting with jdbc:sqlserver://...), or a .NET string (e.g, Data Source=myServerName\myInstance;Database=myDatabase;Integrated Security=false;User ID=sa;Password=<password>;Encrypt=true;TrustServerCertificate=true;).
  * @returns {Promise<{raw: string, json: void}>} This will return a object with the raw string from the database query and a json function to get a parsed version of the query.
  */
-export declare function query(query: string, connection?: string): Promise<unknown>;
+export declare function query(query: string, connection?: string): Promise<{
+    raw: string;
+    json: () => string;
+}>;
 /**
  *
  * @returns {Promise<string>} This will return the default connection string provided in the Tauri Rust setup.
