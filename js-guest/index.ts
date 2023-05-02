0be1446d89ba9949a8f72016a6e59796906ffcf7
Ryan Pell <ryan@ryanpell.co.uk>
2023-05-02 12:46:56 +0100

    change to promises

--- js-guest/index.ts
+++ js-guest/index.ts
@@ -6,16 +6,20 @@ import { invoke } from '@tauri-apps/api/tauri'
  * @param {string} connection This is a optional connection string that you can use to connect to another database that is not the default. The default is the one that is defined within the Tauri setup. The string should be used as as a JDBC string (starting with jdbc:sqlserver://...), or a .NET string (e.g, Data Source=myServerName\myInstance;Database=myDatabase;Integrated Security=false;User ID=sa;Password=<password>;Encrypt=true;TrustServerCertificate=true;). 
  * @returns {Promise<{raw: string, json: void}>} This will return a object with the raw string from the database query and a json function to get a parsed version of the query.
  */
-export async function query (query: string, connection?: string) {
-  let options = {tsql: query} as { tsql: string, connection?: string}
+export function query (query: string, connection?: string) {
+  return new Promise((resolve, reject) => {
+    let options = {tsql: query} as { tsql: string, connection?: string}
+    if(connection){ options = { ...options, connection } } //add connection string to options
 
-  if(connection){ options = { ...options, connection } } //add connection string to options
-
-  let returns: string = await invoke('plugin:mssql|query', options);
-  return {
-    raw: returns,
-    json: () => JSON.parse(returns)
-  }
+    invoke('plugin:mssql|query', options)
+      .then((query: any) => {
+        resolve({
+          raw: query,
+          json: () => JSON.parse(query.toString())
+        })
+      })
+      .catch(err => reject(err))
+  })  
 }
 
 /**
@@ -25,7 +29,7 @@ export async function query (query: string, connection?: string) {
 export function defaultConnectionString () {
   return new Promise<string>((resolve, reject) => {
     invoke('plugin:mssql|default_config')
-      .then((config: any) => resolve(JSON.parse(config)))
-      .catch(error => {console.error(error)})
+      .then((config: any) => resolve(config))
+      .catch(err => reject(err))
   })
 }
